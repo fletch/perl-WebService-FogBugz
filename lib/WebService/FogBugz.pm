4d5a510737a4d50d561ffe18ee3d52aa8423d78d
Barbie <barbie@missbarbell.co.uk>
2014-12-27 18:39:32 +0000

    documentation updates

--- lib/WebService/FogBugz.pm
+++ lib/WebService/FogBugz.pm
@@ -203,8 +203,9 @@ Current Maintainer: Barbie  C<< <barbie@cpan.org> >>
 
 =head1 LICENCE AND COPYRIGHT
 
-Copyright (c) 2007-2014, Takatsugu Shigeta C<< <shigeta@cpan.org> >>. All rights reserved.
-Copyright (c) 2014-2015, Barbie C<< <babrie@cpan.org> >>. All rights reserved.
+  Copyright (c) 2007-2014, Takatsugu Shigeta C<< <shigeta@cpan.org> >>.
+  Copyright (c) 2014-2015, Barbie C<< <babrie@cpan.org> >>. 
+  All rights reserved.
 
 This distribution is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself. See L<perlartistic>.
